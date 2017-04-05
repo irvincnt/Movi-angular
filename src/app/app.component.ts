8ff169b1cc249dd27ee2e32523bce7fdc3de7d01
Irvin Contreras <irvin@watr.mx>
2017-04-05 17:19:41 -0500

    primer commit / conexión a la api de marvel y muestra de nombres e imágenes de super heroes sin estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  personajes;
+  img_corta="/portrait_xlarge.";
   title = 'app works!';
+  
+  constructor(private http: Http){}
+
+  ngOnInit(){
+    this.http.get("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=332d7d0388b04738ed71de41a25745a8&hash=19ca5648a47abb71d1b27d6542691237")
+      .subscribe(data => {
+        this.personajes = data.json().data.results;
+         console.log(this.personajes);
+      });
+  }
 }
